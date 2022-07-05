ea3ff15b80565a66b79985bd25fdf0a3ff6f38a4
Colerar <233hbj@gmail.com>
2022-07-05 14:14:08 +0800

    add '--icons'

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -10,11 +10,11 @@
 
 # general use
 alias ls='exa --icons'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias l='exa --icons -lbF --git'                                               # list, size, type, git
+alias ll='exa --icons -lbGF --git'                                             # long list
+alias llm='exa --icons -lbGF --git --sort=modified'                            # long list, modified date sort
+alias la='exa --icons -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='exa --icons -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 
 # speciality views
 alias lS='exa -1'			                                                  # one column, just names
